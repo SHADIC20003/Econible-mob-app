50fcae7c5e22053526c7ebd0779da597406a7a57
Sherifrax <sherifrax@gmail.com>
2024-06-18 18:23:29 +0300

    Set Budget & modifications

--- lib/view/login/sign_in_view.dart
+++ lib/view/login/sign_in_view.dart
@@ -1,144 +1,132 @@
 import 'package:flutter/material.dart';
 import 'package:trackizer/sqldb.dart';
+import 'package:trackizer/view/home/home_view.dart';
 import 'package:trackizer/view/login/sign_up_view.dart';
 import 'package:trackizer/view/login/social_login.dart';
+import 'package:trackizer/view/login/welcome_view.dart';
+import 'package:trackizer/view/main_tab/main_tab_view.dart';
 
 import '../../common/color_extension.dart';
 import '../../common_widget/primary_button.dart';
-import '../../common_widget/round_textfield.dart';
 import '../../common_widget/secondary_boutton.dart';
+import 'package:trackizer/view/expenses/set.dart';
 
 class SignInView extends StatefulWidget {
-  const SignInView({super.key});
+  const SignInView({Key? key}) : super(key: key);
 
   @override
   State<SignInView> createState() => _SignInViewState();
 }
 
 class _SignInViewState extends State<SignInView> {
-  TextEditingController txtEmail = TextEditingController();
-  TextEditingController txtPassword = TextEditingController();
+  TextEditingController userEmail = TextEditingController();
+  TextEditingController userPassword = TextEditingController();
   bool isRemember = false;
-  bool _isVisible=false;
+  bool _isVisible = false;
 
   SqlDb sqldb = SqlDb();
 
-TextEditingController userEmail = TextEditingController();
-TextEditingController userPassword = TextEditingController();
+  bool _isEmailWarn = false;
+  bool _isPasswordWarn = false;
+  bool _isPasswordCorrect = false;
 
-bool _isEmailWarn=false;
-bool _ispasswordWarn=false;
-//bool _isEmailCorrect = false;
-bool _isPasswordCorrect = false;
+  void checkPassword(String textPassword, String dbPassword) {
+    _isPasswordCorrect = false;
 
-/*checkEmail(textEmail,dbEmail){
-  _isEmailCorrect=false;
-
-  textEmail=textEmail.trim();
-  dbEmail =dbEmail.trim();
-
-  if(textEmail==dbEmail){
-    _isEmailCorrect=true;
+    if (textPassword == dbPassword) {
+      _isPasswordCorrect = true;
+    }
   }
-}*/
 
-checkPassword(textPassword,dbPassword){
-  _isPasswordCorrect=false;
-
-  if(textPassword==dbPassword){
-    _isPasswordCorrect=true;
-  }
-}
   @override
   Widget build(BuildContext context) {
-    var media = MediaQuery.sizeOf(context);
+    var media = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: TColor.gray,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 50),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset("assets/img/eco3.png",
-                  width: media.width * 0.5, fit: BoxFit.contain),
-              const Spacer(),
-               SizedBox(
-                height: 10,
+              Image.asset(
+                "assets/img/eco3.png",
+                width: media.width * 0.5,
+                fit: BoxFit.contain,
               ),
-               const SizedBox(
-                height: 45,
-               child: Text("please enter your email and password",style: TextStyle(color:Color.fromARGB(255, 188, 68, 2)),),
-              ),
+              SizedBox(height: 30), // Adjusted spacing from the logo to text
               SizedBox(
-                height: 10,
+                height: 45,
+                child: Text(
+                  "Please enter your email and password to continue.",
+                  style: TextStyle(color: Color.fromARGB(255, 188, 68, 2)),
+                ),
               ),
-            TextField(
+              SizedBox(height: 20), // Spacing after text
+              TextField(
                 controller: userEmail,
-                cursorColor: const Color.fromARGB(255, 188, 68, 2) ,
+                cursorColor: const Color.fromARGB(255, 188, 68, 2),
                 style: TextStyle(color: TColor.white, fontSize: 14),
                 decoration: InputDecoration(
-                 
                   enabledBorder: const OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
-                      ),
+                    borderSide: BorderSide(color: Colors.grey, width: 0.0),
+                  ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Color.fromARGB(255, 107, 104, 102))
+                    borderSide: const BorderSide(color: Color.fromARGB(255, 107, 104, 102)),
                   ),
-                  
-                  hintText:"Email Ex:ABC@example.com",
-                  hintStyle: const TextStyle(color:Color.fromARGB(255, 188, 68, 2)),
-                
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                  hintText: "Enter your email (e.g., abc@gmail.com)",
+                  hintStyle: const TextStyle(color: Color.fromARGB(255, 188, 68, 2)),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 ),
-                
-               
               ),
-              if(_isEmailWarn==true)
-                 Text("wrong email address ",style: TextStyle(color:Color.fromARGB(255, 188, 68, 2)),),
-              const SizedBox(
-
-                height: 45,
-              // child: Text("please enter the email in the correct form",style: TextStyle(color: _isEmailWritternCorrect ? Color.fromARGB(255, 188, 68, 2) : Colors.transparent,),),
-              ),
-              
+              if (_isEmailWarn)
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    "Wrong email address!",
+                    style: TextStyle(color: Color.fromARGB(255, 188, 68, 2)),
+                  ),
+                ),
+              SizedBox(height: 20), // Spacing after email field or warning
               TextField(
                 controller: userPassword,
-                cursorColor: const Color.fromARGB(255, 188, 68, 2) ,
+                cursorColor: const Color.fromARGB(255, 188, 68, 2),
                 style: TextStyle(color: TColor.white, fontSize: 14),
                 decoration: InputDecoration(
                   suffixIcon: IconButton(
-                    onPressed: (){
+                    onPressed: () {
                       setState(() {
                         _isVisible = !_isVisible;
                       });
-                    }, 
-                    icon: _isVisible? const Icon(Icons.visibility,color: Color.fromARGB(255, 188, 68, 2),) : const Icon(Icons.visibility_off,color: Color.fromARGB(255, 188, 68, 2))
+                    },
+                    icon: _isVisible
+                        ? const Icon(Icons.visibility, color: Color.fromARGB(255, 188, 68, 2))
+                        : const Icon(Icons.visibility_off, color: Color.fromARGB(255, 188, 68, 2)),
                   ),
-                  
                   enabledBorder: const OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
-                      ),
+                    borderSide: BorderSide(color: Colors.grey, width: 0.0),
+                  ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Color.fromARGB(255, 107, 104, 102))
+                    borderSide: const BorderSide(color: Color.fromARGB(255, 107, 104, 102)),
                   ),
-                  hintText:"password Ex:********",
-                  hintStyle: const TextStyle(color:Color.fromARGB(255, 188, 68, 2)),
-                
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                  hintText: "Enter your password (e.g., P@ssw0rd)",
+                  hintStyle: const TextStyle(color: Color.fromARGB(255, 188, 68, 2)),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 ),
-                
                 obscureText: !_isVisible,
               ),
-              if(_ispasswordWarn==true)
-                 Text("password is incorrect",style: TextStyle(color:Color.fromARGB(255, 188, 68, 2)),),
-
-               const SizedBox(
-                height: 20,
-              ),
-
+              if (_isPasswordWarn)
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    "The password you've entered is incorrect, please try again.",
+                    style: TextStyle(color: Color.fromARGB(255, 188, 68, 2)),
+                  ),
+                ),
+              SizedBox(height: 20), // Spacing after password field or warning
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -152,15 +140,11 @@ checkPassword(textPassword,dbPassword){
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         Icon(
-                          isRemember
-                              ? Icons.check_box_rounded
-                              : Icons.check_box_outline_blank_rounded,
+                          isRemember ? Icons.check_box_rounded : Icons.check_box_outline_blank_rounded,
                           size: 25,
                           color: Color.fromARGB(255, 188, 68, 2),
                         ),
-                        const SizedBox(
-                          width: 8,
-                        ),
+                        SizedBox(width: 8),
                         Text(
                           "Remember me",
                           style: TextStyle(color: TColor.gray50, fontSize: 14),
@@ -177,67 +161,43 @@ checkPassword(textPassword,dbPassword){
                   ),
                 ],
               ),
-
-              const SizedBox(
-                height: 8,
-              ),
-
+              SizedBox(height: 8), // Spacing after remember me and forgot password row
               PrimaryButton(
                 title: "Sign In",
-                onPressed: () async{
-                  
-              _ispasswordWarn=false;
-              _isEmailWarn=false;
+                onPressed: () async {
+                  _isPasswordWarn = false;
+                  _isEmailWarn = false;
 
-                  List data = await sqldb.readData("SELECT * FROM users WHERE email = '${userEmail.text.trim()}'");
+                  List<Map<String, dynamic>> data =
+                      await sqldb.readData("SELECT * FROM users WHERE email = '${userEmail.text.trim()}'");
 
-                 
-                  if (data.isNotEmpty){
-                    _isEmailWarn=false;
-                  //String dbEmail = data[0]['email'];
-                  String dbPassword = data[0]['password'];
-                  /*_isEmailCorrect=true;
-                  _isPasswordCorrect=false;*/
-                  checkPassword(userPassword.text, dbPassword);
-                        if(_isPasswordCorrect==true){
-                            Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                            builder: (context) => const SocialLoginView(),
-                                         ),
-                                          );
-                        }
-                        else{
-                          _ispasswordWarn=true;
-                        }
+                  if (data.isNotEmpty) {
+                    String dbPassword = data[0]['password'];
+                    checkPassword(userPassword.text, dbPassword);
 
-                    
+                    if (_isPasswordCorrect) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MainTabView(),
+                        ),
+                      );
+                    } else {
+                      _isPasswordWarn = true;
+                    }
+                  } else {
+                    _isEmailWarn = true;
                   }
-                  else{
-                    _isEmailWarn=true;
-                  }
-                  
-
-                 
-
-                  
-                   /*Navigator.push(
-                     context,
-                     MaterialPageRoute(
-                       builder: (context) => const SocialLoginView(),
-                     ),
-                   );*/
+                  setState(() {});
                 },
               ),
-              const Spacer(),
+              Spacer(),
               Text(
-                "if you don't have an account yet?",
+                "Don't have an account yet?",
                 textAlign: TextAlign.center,
                 style: TextStyle(color: TColor.white, fontSize: 14),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20), // Spacing after "Don't have an account yet?" text
               SecondaryButton(
                 title: "Sign up",
                 onPressed: () {
@@ -249,6 +209,7 @@ checkPassword(textPassword,dbPassword){
                   );
                 },
               ),
+              SizedBox(height: 20), // Spacing at the bottom
             ],
           ),
         ),
